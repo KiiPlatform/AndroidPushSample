9de0d8c2b0ad1c15cc3b38c209b30cb2e226b120
shapan_ecs@yahoo.com <moshiur.rahman@kii.com>
2013-02-18 18:52:32 +0900

    Issue #2 Implement file operations

--- src/com/kii/push/FileBucketPushActivity.java
+++ src/com/kii/push/FileBucketPushActivity.java
@@ -1,14 +1,12 @@
 package com.kii.push;
 
-import com.kii.cloud.storage.Kii;
-import com.kii.cloud.storage.KiiFile;
-import com.kii.cloud.storage.KiiFileBucket;
-import com.kii.cloud.storage.KiiSubscribable;
-import com.kii.cloud.storage.KiiUser;
-import com.kii.cloud.storage.callback.KiiFileCallBack;
-import com.kii.cloud.storage.callback.KiiPushCallBack;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
@@ -17,10 +15,25 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.kii.cloud.storage.Kii;
+import com.kii.cloud.storage.KiiACL;
+import com.kii.cloud.storage.KiiACL.FileAction;
+import com.kii.cloud.storage.KiiACLEntry;
+import com.kii.cloud.storage.KiiAnyAuthenticatedUser;
+import com.kii.cloud.storage.KiiFile;
+import com.kii.cloud.storage.KiiFileBucket;
+import com.kii.cloud.storage.KiiSubscribable;
+import com.kii.cloud.storage.KiiUser;
+import com.kii.cloud.storage.callback.KiiACLCallBack;
+import com.kii.cloud.storage.callback.KiiFileCallBack;
+import com.kii.cloud.storage.callback.KiiPushCallBack;
+
 public class FileBucketPushActivity extends FragmentActivity implements
         OnItemClickListener {
 
     private static final String FILE_BUCKET_NAME = "testFileBucket";
+    private static final String FILE_PATH = "com.kii.push/test_file.txt";
+    private KiiFile mFile = null;
 
     @Override
     protected void onCreate(Bundle arg0) {
@@ -68,25 +81,23 @@ public class FileBucketPushActivity extends FragmentActivity implements
                     public void onItemClick(AdapterView<?> parent, View view,
                             int pos, long id) {
                         if (pos == 0 ) { // Create file in bucket.
-                            KiiFile f = Kii.fileBucket(FILE_BUCKET_NAME).file();
-                            f.save(new KiiFileCallBack() {
-                                @Override
-                                public void onSaveCompleted(int token,
-                                        KiiFile file, Exception exception) {
-                                    if (exception != null) {
-                                        // TODO: impl progress.
-                                        showAlertDialog(exception.getMessage());
-                                    } else {
-                                        Toast.makeText(getApplicationContext(),
-                                                "Create file suceeded",
-                                                Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        } else {
-                            // TODO: implement other menu.
+                            doCreateFile();
+                        } else if(pos == 1) {
+                            doUpdateFileMetaData();
+                        } else if(pos == 2) {
+                            doUpdateFileBody();
+                        } else if(pos == 3) {
+                            doDeleteFile();
+                        } else if(pos == 4) {
+                            doMoveFileToTrash();
+                        } else if(pos == 5) {
+                            doUpdateFileACL(); 
+                        } else if(pos == 6) {
+                            doRestoreFileFromTrash();
+                        }
+                        else {
                             Toast.makeText(getApplicationContext(),
-                                    "Not implemented yet.", Toast.LENGTH_LONG)
+                                    "unknown option.", Toast.LENGTH_LONG)
                                     .show();
                         }
                         dismissDialogByTag(ListDialogFragment.TAG);
@@ -95,11 +106,195 @@ public class FileBucketPushActivity extends FragmentActivity implements
         frag.show(this.getSupportFragmentManager(), ListDialogFragment.TAG);
     }
 
+    private void doCreateFile() {
+        mFile = Kii.fileBucket(FILE_BUCKET_NAME).file();
+        mFile.save(new KiiFileCallBack() {
+            @Override
+            public void onSaveCompleted(int token,
+                    KiiFile file, Exception exception) {
+                if (exception != null) {
+                    // TODO: impl progress.
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Create file suceeded",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void doUpdateFileMetaData() {
+        if(mFile == null) {
+            Toast.makeText(getApplicationContext(),
+                    "File is not created yet.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        mFile.setCustomeField("custom"+System.currentTimeMillis());
+        mFile.save(new KiiFileCallBack() {
+            @Override
+            public void onSaveCompleted(int token,
+                    KiiFile file, Exception exception) {
+                if (exception != null) {
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "update meta-data suceeded",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void doUpdateFileBody() {
+        if(mFile == null) {
+            Toast.makeText(getApplicationContext(),
+                    "File is not created yet.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        mFile.save(new KiiFileCallBack() {
+            @Override
+            public void onSaveCompleted(int token,
+                    KiiFile file, Exception exception) {
+                if (exception != null) {
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "update meta-data suceeded",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, getFile());
+    }
+
+    private void doDeleteFile() {
+        if(mFile == null) {
+            Toast.makeText(getApplicationContext(),
+                    "File is not created yet.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        mFile.delete(new KiiFileCallBack() {
+            @Override
+            public void onDeleteCompleted(int token, Exception exception) {
+                if (exception != null) {
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    mFile = null;
+                    Toast.makeText(getApplicationContext(),
+                            "file delete suceeded",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void doMoveFileToTrash() {
+        if(mFile == null) {
+            Toast.makeText(getApplicationContext(),
+                    "File is not created yet.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        mFile.moveToTrash(new KiiFileCallBack() {
+            @Override
+            public void onMoveTrashCompleted(int token, KiiFile file,
+                    Exception exception) {
+                if (exception != null) {
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "file move to trash suceeded",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        
+        });
+    }
+
+    private void doUpdateFileACL() {
+        if (mFile == null) {
+            Toast.makeText(getApplicationContext(), "File is not created yet.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        KiiACL acl = mFile.acl();
+        KiiACLEntry entry = new KiiACLEntry(KiiAnyAuthenticatedUser.create(),
+                FileAction.READ_EXISTING_OBJECT, true);
+        acl.putACLEntry(entry);
+        acl.save(new KiiACLCallBack() {
+
+            @Override
+            public void onSaveCompleted(int token, KiiACL acl,
+                    Exception exception) {
+                if (exception != null) {
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "file acl change succeeded", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+
+        });
+    }
+
+    private void doRestoreFileFromTrash() {
+        if(mFile == null) {
+            Toast.makeText(getApplicationContext(),
+                    "File is not created yet.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        mFile.restoreFromTrash(new KiiFileCallBack() {
+            @Override
+            public void onRestoreTrashCompleted(int token, KiiFile file,
+                    Exception exception) {
+                if (exception != null) {
+                    showAlertDialog(exception.getMessage());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "file restore from trash suceeded",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        
+        });
+    }
+
+
     private void dismissDialogByTag(String tag) {
         DialogFragment df = (DialogFragment) FileBucketPushActivity.this
                 .getSupportFragmentManager().findFragmentByTag(
                         ListDialogFragment.TAG);
         df.dismiss();
     }
+ 
+    private File getFile() {
+        File file = new File(Environment.getExternalStorageDirectory(),
+                FILE_PATH);
+        BufferedWriter bw = null;
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            bw = new BufferedWriter(new FileWriter(file, true));
+            bw.write("hogehoge" + System.currentTimeMillis());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+        return file;
+    }
+
 
 }
